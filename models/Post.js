6b3718c4b921d7a97017662e74297d75853543f9
austin030606 <austin030606@gmail.com>
2022-04-30 20:13:48 +0800

    feat: add Post.js

---
+++ models/Post.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const PostSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  slide: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: [String],
+  },
+  thumbnail_path: {
+    type: String,
+    required: true,
+    default: ''
+  },
+  team_id: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'Team'
+  }
+});
+
+export default mongoose.model('Post', PostSchema);
