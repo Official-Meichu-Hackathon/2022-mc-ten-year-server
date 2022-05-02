35660c54795241fd441472beb692122b123a5a6c
bosyuan <bosyuanhou@gmail.com>
2022-05-02 11:00:54 +0800

    feat: add competitor.js

---
+++ models/competitor.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const CompetitorSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  phone_number: {
+    type: String,
+    required: false
+  },
+  email_address: {
+    type: String,
+    required: false
+  },
+  feedback: {
+    type: String,
+    required: false
+  },
+  team_id: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'Team' 
+  },
+});
+
+
+export default mongoose.model('Competitor', CompetitorSchema);
