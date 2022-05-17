248ee8a66112c3c1a7365947b27fd88566548c1d
r1407p <buszter0403@gmail.com>
2022-05-17 11:59:55 +0800

    fix: routes/question

--- routes/question.js
+++ routes/question.js
@@ -4,7 +4,7 @@ import auth from '../middleware/auth';
 
 const questionRouter = express.Router();
 
-questionRouter.post('/addQuestion', controller.question.addQuestion);
+questionRouter.post('/addQuestion', auth(), controller.question.addQuestion);
 questionRouter.post('/getQuestion', controller.question.getQuestion);
 questionRouter.post('/getQuestions', controller.question.getQuestions);
 questionRouter.post('/modifyQuestion', auth(), controller.question.modifyQuestion);
