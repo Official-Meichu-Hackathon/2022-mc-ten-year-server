4e7b04d84fc7c9e3a39148029d464abd4d129d23
ting0602 <69136310+ting0602@users.noreply.github.com>
2022-05-28 21:36:36 +0800

    feat: add login in post unit test

--- test/unit/service/post.spec.js
+++ test/unit/service/post.spec.js
@@ -15,10 +15,28 @@ describe('Test \'post\' service', () => {
   });
 
   let newPost = null;
+  // gain token
+  let token = null;
+  // let newUser = null;
+
+  describe('Test \'users.login\' action', () => {
+    it('login, should return with a token', async () => {
+      const res = await request(app).post('/user/login').send({
+        username: 'testuser',
+        password: 'rootroot'
+      });
+      expect(res.body).toEqual(expect.objectContaining({
+        token: expect.anything()
+      }));
+      token = res.body.token;
+    });
+  });
+  // get token
 
   describe('Test \'post.addPost\' action', () => {
     it('add a post, should return with the post', async () => {
       const res = await request(app).post('/post/addPost')
+        .set('Authorization', `Bearer ${token}`)
         .send({
           name: 'postName',
           description: 'This is a long description.',
@@ -45,6 +63,7 @@ describe('Test \'post\' service', () => {
   describe('Test \'post.addPost\' action', () => {
     it('add a post, should return with the post', async () => {
       const res = await request(app).post('/post/addPost')
+        .set('Authorization', `Bearer ${token}`)
         .send({
           name: 'postName',
           description: 'This is a long description.',
@@ -69,6 +88,7 @@ describe('Test \'post\' service', () => {
   describe('Test \'post.getPost\' action', () => {
     it('get a post, should return with the post', async () => {
       const res = await request(app).post('/post/getPost')
+        .set('Authorization', `Bearer ${token}`)
         .send({ _id: newPost._id });
       expect(res.body).toEqual(expect.objectContaining({
         _id: newPost._id,
@@ -86,6 +106,7 @@ describe('Test \'post\' service', () => {
   describe('Test \'post.getPosts\' action', () => {
     it('get posts, should return with the post list', async () => {
       const res = await request(app).post('/post/getPosts')
+        .set('Authorization', `Bearer ${token}`)
         .send({ filter: { name: 'postName' } });
       expect(res.body).toHaveProperty('total');
       expect(res.body.total).toBeGreaterThanOrEqual(2);
@@ -99,6 +120,7 @@ describe('Test \'post\' service', () => {
   describe('Test \'post.modifyPost\' action', () => {
     it('modify a post, should return with \'success message\'', async () => {
       const res = await request(app).post('/post/modifyPost')
+        .set('Authorization', `Bearer ${token}`)
         .send({ _id: newPost._id, description: 'This is a new long description.' });
       expect(res.body).toHaveProperty('success', true);
     });
@@ -107,6 +129,7 @@ describe('Test \'post\' service', () => {
   describe('Test \'post.removePost\' action', () => {
     it('remove a post, should return with \'success message\'', async () => {
       const res = await request(app).post('/post/removePost')
+        .set('Authorization', `Bearer ${token}`)
         .send({ _id: newPost._id });
       expect(res.body).toHaveProperty('success', true);
     });
@@ -115,6 +138,7 @@ describe('Test \'post\' service', () => {
   describe('Test \'post.removePosts\' action', () => {
     it('remove posts, should return with \'success message\'', async () => {
       const res = await request(app).post('/post/removePosts')
+        .set('Authorization', `Bearer ${token}`)
         .send({ team_id: '61d634706a98a61111111111' });
       expect(res.body.deletedCount).toBeGreaterThanOrEqual(0);
     });
