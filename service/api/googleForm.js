bf1f441026d26cfa344dcf09177ad0d7be68f23f
icesplendent <icesplendent@gmail.com>
2022-06-07 14:14:51 +0800

    fix: remove redundant code

--- service/api/googleForm.js
+++ service/api/googleForm.js
@@ -1,16 +1,10 @@
 import path from 'path';
 import google from '@googleapis/forms';
-// import { authenticate } from '@google-cloud/local-auth';
 
 const formID = '1dWCqmthEMLuhH-_226gjYLr-XTgTmeGT5pM8Q74VqZ0';
 const __dirname = process.cwd();
 
 async function getData() {
-  // const auth = await authenticate({
-  //   keyfilePath: path.join(__dirname, '/libs/credentials.json'),
-  //   scopes: 'https://www.googleapis.com/auth/forms.responses.readonly'
-  // });
-
   const auth = new google.auth.GoogleAuth({
     keyFilename: path.join(__dirname, '/credentials.json'),
     scopes: [
