1c15673ff047e7ce9811ad4d079bffa5a7e8213e
icesplendent <icesplendent@gmail.com>
2022-08-14 14:22:20 +0800

    fix: remove redundant code

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import { google } from 'googleapis';
 import logger from './libs/logger';
 import router from './routes';
 import connectMongo from './libs/connect_mongo';
@@ -19,35 +18,7 @@ app.use(express.raw({ type: 'image/png', limit: '10mb' }));
 // Router
 app.use(router);
 app.get('/', async (req, res) => {
-  const auth = new google.auth.GoogleAuth({
-    keyFile: 'credentials.json',
-    scopes: 'https://www.googleapis.com/auth/spreadsheets'
-  });
-
-  // create client instance for auth
-  const client = await auth.getClient();
-
-  const spreadsheetId = '1f4xbdBe3P5e0MA5T-F8wqRGgDR9UCQ_QkYm08ewd-Ek';
-
-  // instance of Google Sheet API
-  const googleSheets = google.sheets({ version: 'v4', auth: client });
-
-  // Get metadata about spreadsheet
-  const metaData = await googleSheets.spreadsheets.get({
-    auth,
-    spreadsheetId
-  });
-
-  // get Rows
-  const getRows = await googleSheets.spreadsheets.values.get({
-    auth,
-    spreadsheetId,
-    range: 'Sheet1'
-  });
-
-  res.json({ data: getRows.data, dataSet: getRows.data.values.length - 1 });
-
-  // res.json({ message: 'Welcome to MC-Ten-year-Server!' });
+  res.json({ message: 'Welcome to MC-Ten-year-Server!' });
 });
 
 if (process.env.NODE_ENV !== 'test') {
